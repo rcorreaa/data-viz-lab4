e23d54f7544282575c5aa5a74b045b0d3169ee6a
Ramyro Corrêa Aquines <ramyrocorrea@gmail.com>
2025-03-20 12:54:14 -0300

    lab-4

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<h1 style="color:deeppink;"> Ramyro </h1>
+
+<p> Fala rapazeada, Ramyro na área!</p>
+
+<img src="images/my_image.jpeg" alt="A description of the image" width="400">
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,10 @@
+<section>
+    <h2>Entre em contato</h2>
+    <p>Contate-me através dos seguintes meios:</p>
+    
+    <ul>
+        <li><strong>Email:</strong> <a href="mailto:ramyrocorrea@gmail.com">ramyrocorrea@gmail.com</a></li>
+        <li><strong>LinkedIn:</strong> <a href="https://www.linkedin.com/in/ramyro-correa-aquines/" target="_blank">LinkedIn</a></li>
+        <li><strong>GitHub:</strong> <a href="https://github.com/rcorreaa" target="_blank">GitHub</a></li>
+    </ul>
+</section>
